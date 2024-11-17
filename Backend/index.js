adaa819f949a78836a12eee7a773e8e5de95214a
Jjagla <jaglanshristi@gmail.com>
2024-11-17 15:18:49 +0530

    message

--- Backend/index.js
+++ Backend/index.js
@@ -3,16 +3,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.route.js";
-import companyRoute from "./routes/company.route.js";
-import jobRoute from "./routes/job.route.js";
-import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
 const app = express();
 
-// middleware
+
+
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
@@ -23,18 +21,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+
 const PORT = process.env.PORT || 3000;
 
 
-// api's
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute);
-app.use("/api/v1/application", applicationRoute);
-
-
-
 app.listen(PORT,()=>{
-    connectDB();
+    connectDB(); 
     console.log(`Server running at port ${PORT}`);
 })
